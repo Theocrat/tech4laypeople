ac6cf76ee7453549bd8164de28137d5c7e9ea37e
Rajarshi Bandopadhyay <theocrat93@gmail.com>
2022-03-08 01:47:24 +0530

    Decent about page, JS scripts or content list

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 This is a blog which I intend to publish on Github pages, mainly to test the possibilities afforded by Github for content publishing and custom domain name. Instead of publishing it as a series of markdown files, I have decided to publish this entire document as an HTML blog. I shall be writing the HTML, CSS and Javascript code for this project as well.
 
+Another purpose of this blog is to spread awareness about the way advanced technology in our times work. This will lower the barrier of information asymmetry and make it easier for lay persons - people without a professional interest in technology - to understand how these technologies work.
+
+Finally, this is my first major project in the domain of HTML5, and I am eager to learn much in front-end UI/UX design.
 
 ### TODO for HTML, CSS and Javascript part
 
@@ -14,4 +17,3 @@ This is a blog which I intend to publish on Github pages, mainly to test the pos
 
   * Write the introduction to AI
   * Write the article on SGD
-
--- js/controls.js
+++ js/controls.js
@@ -27,3 +27,64 @@ function nightmode() {
         togglebutton.innerHTML = 'Night Mode'
     }        
 }
+
+const controlTable = `
+<table class="control-table">
+<tr>
+    <td class="control">
+        <img src="svg/fonts.svg"
+             width="48px" 
+             height="48px"
+             title="Select Font">
+        </img>
+    </td>
+    
+    <td class="control">
+        <img src="svg/size.svg"
+             width="48px" 
+             height="48px"
+             title="Font Size">
+        </img>
+    </td>
+    
+    <td>
+        <img src="svg/nightmode.svg"
+             width="48px" 
+             height="48px"
+             title="Toggle Night Mode">
+        </img>
+    </td>
+</tr>
+
+<tr>
+    <td class="control">
+        <select id="font-selector" onchange="changefont()">
+            <option value="arial">Arial</option>
+            <option value="sans" >Sans </option>
+        </select>
+    </td>
+    
+    <td class="control">
+        <select id="size-selector" onchange="changesize()">
+            <option value="12">12</option>
+            <option value="14">14</option>
+            <option value="16">16</option>
+            <option value="18">18</option>
+            <option value="20">20</option>
+            <option value="22">22</option>
+        </select>
+    </td>
+    
+    <td class="control">
+        <button id="nightmode" onclick="nightmode()">
+            Night Mode
+        </button>
+    </td>
+</tr>
+</table>`
+
+function renderControls() {
+    let controlNode = document.getElementById('controlTable')
+    
+    controlNode.innerHTML = controlTable
+}
